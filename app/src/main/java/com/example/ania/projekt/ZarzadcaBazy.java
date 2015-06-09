3eb2087080c76dd27ed7811ba86135389fa06a9c
kapibara92 <asia_9215@onet.eu>
2015-06-09 19:08:26 +0200

    Dodanie bazy danych

---
+++ app/src/main/java/com/example/ania/projekt/ZarzadcaBazy.java
@@ -0,0 +1,79 @@
+package com.example.ania.projekt;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by Kapibara on 2015-06-09.
+ */
+public class ZarzadcaBazy extends SQLiteOpenHelper {
+    public ZarzadcaBazy(Context context) {
+        super(context, "baza.db", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("create table wymiary(" + "id integer primary key autoincrement, " + "dataZapisu datetime, " +
+                "wzrost double, " + "waga double, " + "obKlatki double, " + "obTalii double, " + "obBioder double, " + "obUda double);" + "");
+        db.execSQL("create table trening(" + "id integer primary key autoincrement, " + "czasTreningu time, " + "rodzajTreningu string, " + "dystans double, "
+                + "kategoria string, " + "notatka string, " + "styl string, " + "data datetime);"+"");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersin, int newVersion )
+    {
+
+        db = getWritableDatabase();
+        db.execSQL("drop table if exists wymiary;");
+        db.execSQL("drop table if exists trening;");
+        onCreate(db);
+    };
+    //metoda zapisuje wymiar uzytkownika wraz z aktualna data zapisu
+    public void dodajWymiar(double wzrost, double waga, double obKlatki,double obTalii,
+                            double obBioder, double obUda)
+    {
+        try {
+            SQLiteDatabase db = getWritableDatabase();
+
+            ContentValues wartosc = new ContentValues();
+            //pobietanie aktualnej daty
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            String strDate = sdf.format(c.getTime());
+            wartosc.put("dataZapisu", strDate);
+            wartosc.put("wzrost", wzrost);
+            wartosc.put("waga", waga);
+            wartosc.put("obKlatki", obKlatki);
+            wartosc.put("obTalii", obTalii);
+            wartosc.put("obBioder", obBioder);
+            wartosc.put("obUda", obUda);
+            db.insertOrThrow("wymiary", null, wartosc);
+            db.close();
+        }
+        catch (Exception e)
+        {
+        }
+    }
+    // dodawanie treningu
+    public void dodajTrening(String data, String czas, String rodzajTreningu,
+                             double Dystans, String Kategoria, String notatka, String Styl){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues wartosc = new ContentValues();
+        wartosc.put("data", data);
+        wartosc.put("czasTreningu", czas);
+        wartosc.put("rodzajTreningu", rodzajTreningu);
+        wartosc.put("dystans", Dystans);
+        wartosc.put("kategoria", Kategoria);
+        wartosc.put("styl", Styl);
+        wartosc.put("notatka",notatka);
+        db.insertOrThrow("trening", null, wartosc);
+
+
+
+    }
+}
--- app/src/main/java/com/example/ania/projekt/rodzaj_Treningu.java
+++ app/src/main/java/com/example/ania/projekt/rodzaj_Treningu.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by Kapibara on 2015-06-02.
  */
 public class rodzaj_Treningu extends Activity {
+    ZarzadcaBazy zb;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dodawanie_terningu);
+        zb=new ZarzadcaBazy(this);
         Button b1;
         Button b2=(Button)findViewById(R.id.anuluj);
         b2.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +42,15 @@ public class rodzaj_Treningu extends Activity {
                                           Intent i = new Intent(getApplicationContext(), Plywanie.class);
                                           startActivity(i);
                                       }
+                                  }
+                              }
+        );
+        Button b3=(Button)findViewById(R.id.Zapisz);
+        b3.setOnClickListener(new View.OnClickListener() {
+                                  public void onClick(View v) {
+                                     // zb.dodajTrening("15-01-2001", "11:23:03", "bieg",
+                                      //23, "fdf", "df", "df");
+                                      Toast.makeText(getApplicationContext(), "zapisano nowy rekord do bazy", Toast.LENGTH_SHORT).show();
                                   }
                               }
         );
