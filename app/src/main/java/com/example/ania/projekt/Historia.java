32ef582c35a285d8b0250ae5b6983e250c8ad669
ninbo <annaa.suwala@gmail.com>
2015-06-14 12:41:33 +0200

    Wyświetlenie  histoirii wymairów oraz aktualnych wymiarów użytkownika

--- app/src/main/java/com/example/ania/projekt/Historia.java
+++ app/src/main/java/com/example/ania/projekt/Historia.java
@@ -1,11 +1,13 @@
 package com.example.ania.projekt;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Ania on 2015-05-24.
@@ -16,7 +18,8 @@ public class Historia extends Fragment {
         zb=new ZarzadcaBazy(getActivity());
          View w = inflater.inflate(R.layout.fragment_historia, container, false);
 
-        Button cliButton = (Button) w .findViewById(R.id.btn_usun_hist_tren);
+        Button cliButton = (Button) w.findViewById(R.id.btn_usun_hist_tren);
+        
 
         cliButton.setOnClickListener(new View.OnClickListener() {
             @Override
